15e78cb74f84fafbea60e1fcf51a2ff12d7014a8
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-04-07 20:10:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    ScrollReveal().reveal('.img-me', {
+        origin: 'up', 
+        duration: 2000,
+        distance: '20%'
+    });
+    }); 
+
+document.addEventListener("DOMContentLoaded", function() {
+    ScrollReveal().reveal('.container', {
+        origin: 'left',
+        duration: 3000,
+        distance: '30%'
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    ScrollReveal().reveal('.social-links', {
+        origin: 'right',
+        duration: 3000,
+        distance: '30%'
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    ScrollReveal().reveal('.social-icons', {
+        origin: 'left',
+        duration: 4000,
+        distance: '40%'
+    });
+});
+
